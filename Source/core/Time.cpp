3a4b0e779c869764af801d13dbf6f21d9c841d00
Lakshmipriya <lakshmipriyap@tataelxsi.co.in>
2020-02-07 10:25:39 +0000

    BD-775 Request To Scan Metrological Github Thunder,Header file changes

--- Source/core/Time.cpp
+++ Source/core/Time.cpp
@@ -1,3 +1,22 @@
+ /*
+ * If not stated otherwise in this file or this component's LICENSE file the
+ * following copyright and licenses apply:
+ *
+ * Copyright 2020 RDK Management
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+ 
 #include "Time.h"
 #include <time.h>
 
@@ -745,6 +764,9 @@ namespace Core {
         return (y2 / 4 - y1 / 4) - (y2 / 100 - y1 / 100) + (y2 / 400 - y1 / 400);
     }
 
+    /*
+     * Code adapted from Python 2.4.1 sources (Lib/calendar.py).
+     */
     static time_t mktimegm(const struct tm* tm)
     {
         int year;
--- cmake/config/core/eval.cmake
+++ cmake/config/core/eval.cmake
@@ -1,4 +1,20 @@
-# Evaluate expression (faster version)
+# If not stated otherwise in this file or this component's license file the
+# following copyright and licenses apply:
+#
+# Copyright 2020 RDK Management
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # Suggestion from the Wiki: http://cmake.org/Wiki/CMake/Language_Syntax
 # Unfortunately, no built-in stuff for this: http://public.kitware.com/Bug/view.php?id=4034
 # eval will not modify ans (the code evaluated may modify ans)
